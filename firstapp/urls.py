4796ae571c7bd091f9ca1bab0247b5aee937c7b2
anoopmanu <anoopmanu96820@gmail.com>
2024-03-11 18:16:07 +0530

    first commit

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,6 +4,7 @@ from .import views
 
 urlpatterns = [
    path('',views.hello,name='hello'),
+   path('dashboard',views.dashboard,name='dashboard'),
   
    
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def hello(request):
     return render(request,'index.html')
+def dashboard(request):
+    return render(request,'dashboard.html')
 
