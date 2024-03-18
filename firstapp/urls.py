3dca44e6b87505b3c42cded9dad85152d964a7ef
anoopmanu <anoopmanu96820@gmail.com>
2024-03-18 17:09:41 +0530

    second commit

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
    path('',views.hello,name='hello'),
    path('dashboard',views.dashboard,name='dashboard'),
+    path('archieve',views.archieve,name='archieve'),
   
    
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 def hello(request):
     return render(request,'index.html')
 def dashboard(request):
-    return render(request,'dashboard.html')
+   dashboard = request.GET.get('dashboard',' ').split(',')
+   return render(request, 'dashboard.html',{'dashboard':dashboard})
+def archieve(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'archieve.html', {'item_ids': item_ids})
 
